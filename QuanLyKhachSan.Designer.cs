243ac9864541e2709dee1f545c62cccf6b6e116b
trangphamtd1 <trangphamtd1@gmail.com>
2020-04-04 10:15:36 -0700

    first commit

---
+++ QuanLyKhachSan.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class QuanLyKhachSan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmaphong = new System.Windows.Forms.TextBox();
+            this.txttenphong = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtdongia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView_qlks = new System.Windows.Forms.DataGridView();
+            this.Maphong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tenphong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dongia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.btnhuy = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_qlks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "mã phòng";
+        
+            // 
+            // txtmaphong
+            // 
+            this.txtmaphong.Location = new System.Drawing.Point(142, 45);
+            this.txtmaphong.Name = "txtmaphong";
+            this.txtmaphong.Size = new System.Drawing.Size(100, 20);
+            this.txtmaphong.TabIndex = 1;
+            // 
+            // txttenphong
+            // 
+            this.txttenphong.Location = new System.Drawing.Point(142, 89);
+            this.txttenphong.Name = "txttenphong";
+            this.txttenphong.Size = new System.Drawing.Size(100, 20);
+            this.txttenphong.TabIndex = 3;
+
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "tên phòng";
+            // 
+            // txtdongia
+            // 
+            this.txtdongia.Location = new System.Drawing.Point(422, 45);
+            this.txtdongia.Name = "txtdongia";
+            this.txtdongia.Size = new System.Drawing.Size(100, 20);
+            this.txtdongia.TabIndex = 5;
+            this.txtdongia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtdongia_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(330, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "đơn giá";
+            // 
+            // dataGridView_qlks
+            // 
+            this.dataGridView_qlks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_qlks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Maphong,
+            this.Tenphong,
+            this.Dongia});
+            this.dataGridView_qlks.Location = new System.Drawing.Point(85, 124);
+            this.dataGridView_qlks.Name = "dataGridView_qlks";
+            this.dataGridView_qlks.Size = new System.Drawing.Size(340, 150);
+            this.dataGridView_qlks.TabIndex = 6;
+            this.dataGridView_qlks.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_qlks_CellClick);
+            // 
+            // Maphong
+            // 
+            this.Maphong.DataPropertyName = "Maphong";
+            this.Maphong.HeaderText = "mã phòng";
+            this.Maphong.Name = "Maphong";
+            // 
+            // Tenphong
+            // 
+            this.Tenphong.DataPropertyName = "Tenphong";
+            this.Tenphong.HeaderText = "tên phòng";
+            this.Tenphong.Name = "Tenphong";
+            // 
+            // Dongia
+            // 
+            this.Dongia.DataPropertyName = "Dongia";
+            this.Dongia.HeaderText = "đơn giá";
+            this.Dongia.Name = "Dongia";
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(12, 307);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 7;
+            this.btnthem.Text = "thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(103, 307);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 8;
+            this.btnxoa.Text = "xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnluu
+            // 
+            this.btnluu.Location = new System.Drawing.Point(195, 307);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(75, 23);
+            this.btnluu.TabIndex = 9;
+            this.btnluu.Text = "lưu";
+            this.btnluu.UseVisualStyleBackColor = true;
+            this.btnluu.Click += new System.EventHandler(this.btnluu_Click);
+            // 
+            // btnhuy
+            // 
+            this.btnhuy.Location = new System.Drawing.Point(298, 307);
+            this.btnhuy.Name = "btnhuy";
+            this.btnhuy.Size = new System.Drawing.Size(75, 23);
+            this.btnhuy.TabIndex = 10;
+            this.btnhuy.Text = "hủy";
+            this.btnhuy.UseVisualStyleBackColor = true;
+            this.btnhuy.Click += new System.EventHandler(this.btnhuy_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(481, 307);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(75, 23);
+            this.btnthoat.TabIndex = 11;
+            this.btnthoat.Text = "thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(390, 307);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 12;
+            this.btnsua.Text = "sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // QuanLyKhachSan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btnhuy);
+            this.Controls.Add(this.btnluu);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.dataGridView_qlks);
+            this.Controls.Add(this.txtdongia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txttenphong);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmaphong);
+            this.Controls.Add(this.label1);
+            this.Name = "QuanLyKhachSan";
+            this.Text = "QuanLyKhachSan";
+            this.Load += new System.EventHandler(this.QuanLyKhachSan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_qlks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmaphong;
+        private System.Windows.Forms.TextBox txttenphong;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdongia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView_qlks;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Maphong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tenphong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dongia;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.Button btnhuy;
+        private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.Button btnsua;
+    }
+}
---
+++ QuanLyKhachSan.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace QuanLyKhachSan
+{
+
+    public partial class QuanLyKhachSan : Form
+    {
+        SqlConnection con = new SqlConnection();
+        public QuanLyKhachSan()
+        {
+            InitializeComponent();
+        }    
+        private void QuanLyKhachSan_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=QuanLyKhachSan;Integrated Security=True";
+            con.ConnectionString = connectionString;
+            con.Open();
+            loaddatatogridview();
+
+        }
+        private void loaddatatogridview()
+        {
+            string sql = "select*from QLKhachsan";
+            SqlDataAdapter adp = new SqlDataAdapter(sql, con);
+            DataTable tableQLKhachsan = new DataTable();
+            adp.Fill(tableQLKhachsan);
+            dataGridView_qlks.DataSource = tableQLKhachsan;
+        }
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            txtmaphong.Enabled = true;
+            txtmaphong.Text = "";
+            txtdongia.Text = "";
+            txttenphong.Text = "";
+        }
+
+        private void dataGridView_qlks_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtdongia.Text = dataGridView_qlks.CurrentRow.Cells["Dongia"].Value.ToString();
+            txtmaphong.Text = dataGridView_qlks.CurrentRow.Cells["Maphong"].Value.ToString();
+            txttenphong.Text = dataGridView_qlks.CurrentRow.Cells["Tenphong"].Value.ToString();
+            txtmaphong.Enabled = false;
+        }
+        private void btnluu_Click(object sender, EventArgs e)
+        {
+            if (txtmaphong.Text == "")
+            {
+                MessageBox.Show("bạn cần nhập mã phòng");
+                txtmaphong.Focus();
+            }
+            if (txttenphong.Text == "")
+            {
+                MessageBox.Show("bạn cần nhập tên phòng");
+                txttenphong.Focus();
+            }
+            if(txtdongia.Text=="")
+            {
+                MessageBox.Show("bạn cần nhập đơn giá");
+                txtdongia.Focus();
+            }    
+            else
+            {
+                string sql = " insert into QLKhachsan values ('" + txtmaphong.Text + "','" + txttenphong.Text + "'";
+                if (txtdongia.Text != "")
+                    sql = sql + "," + txtdongia.Text.Trim();
+                sql = sql + ")";
+                // MessageBox.Show(sql);
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    cmd.ExecuteNonQuery();
+                    loaddatatogridview();
+                }
+                 catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }                               
+                   return;
+            }
+        }
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            string sql = "delete from QLKhachsan where Maphong='" + txtmaphong.Text + "'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            loaddatatogridview();
+        }
+        private void txtdongia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) ||
+                (e.KeyChar == '.') || (Convert.ToInt32(e.KeyChar) == 8) || (Convert.ToInt32(e.KeyChar) == 13))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+        private void btnhuy_Click(object sender, EventArgs e)
+        {       
+            txtmaphong.Text = "";
+            txtdongia.Text = "";
+            txttenphong.Text = "";
+        } 
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            this.Close();
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            string sql = " update QLKhachsan set Tenphong= '" + txttenphong.Text + "',Dongia='" + txtdongia.Text + "' where Maphong='" + txtmaphong.Text + "'";
+            if (txtdongia.Text == "")
+            {
+                MessageBox.Show("Bạn cần nhập đơn giá");
+                txtdongia.Focus();
+                return;
+            }
+            if (txttenphong.Text == "")
+            {
+                MessageBox.Show("Bạn cần nhập tên phòng");
+                txtdongia.Focus();
+                return;
+            } 
+            
+            txtmaphong.Enabled = false;
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            loaddatatogridview();
+        }
+    }
+}
+
